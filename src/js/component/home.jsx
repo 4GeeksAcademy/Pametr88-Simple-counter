58e332c254888e46bc38512d3f345cce230a6796
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-11-17 19:28:30 +0000

    agregue estilos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className = "container row counter">
+				<div className="col reloj"><i className="far fa-clock"></i></div>
+				<div className="col reloj">{props.digicSix}</div>
+				<div className="col reloj">{props.digicFive}</div>
+				<div className="col reloj">{props.digicFour}</div>
+				<div className="col reloj">{props.digicThree}</div>
+				<div className="col reloj">{props.digicTwo}</div>
+				<div className="col reloj">{props.digicOne}</div>
+			</div>
 		</div>
-	);
+	) 
+
+
+
+
+	// const [unit, setUnit] = useState (0);
+	
+	// useEffect(() => {
+	// 	const interval = setInterval(() => {
+	// 		setUnit((prevUnit) => (prevUnit + 1) % 10000);
+	// 	}, 1000);
+	// 	return () => {
+	// 		clearInterval(interval);
+	// 	};
+	// },[]);
+
+	// const formattedUnit = String(unit).padStart(4, '0');
+	
+	// return (
+	// 	<div className="text-center">
+			
+	// 		<p>{unit}</p>
+
+	// 	</div>
+	// );
 };
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,16 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let segundo = 0;
+setInterval(()=>{
+const one = Math.floor((segundo/1)%10)
+const two = Math.floor((segundo/10)%10)
+const three = Math.floor((segundo/100)%10)
+const four = Math.floor((segundo/1000)%10)
+const five = Math.floor((segundo/10000)%10)
+const six = Math.floor((segundo/100000)%10)
+segundo +=1
+ReactDOM.render(<Home digicOne={one} digicTwo={two} digicThree={three} digicFour={four} digicFive={five} digicSix={six}/>, document.querySelector("#app"));
+},1000)
+
+
